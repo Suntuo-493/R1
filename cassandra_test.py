6d0bef1486eefa1c8fdfd99b44d9aabe3c54e867
Suntuo-493 <49358361+Suntuo-493@users.noreply.github.com>
2019-04-07 16:52:14 +0800

    Update cassandra_test.py

--- cassandra_test.py
+++ cassandra_test.py
@@ -9,14 +9,22 @@
 #  port:9042
 #  use the --link name_of_container:name_of_link
 ##################################################
+#  in the model this code will be changed in to
+#  a function. the input value is
+#  1. the return of the model
+#  2. the return of image_prepare
+##################################################
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
 import datetime
 import numpy as np
 KEYSPACE = "mykeyspace"
+
+# the value below is just for test the function
 predictresult = 4
 image_arr = [2, 7, 3, 5, 1, 3, 1, 1, 1, 2, 3, 4, 4, 5, 3, 4, 4, 4, 3, 1, 2, 3]
 image_info= ','.join(str(i) for i in image_arr)
+information_value = image_info
 
 #connnect to the cassandra
 cluster = Cluster(contact_points=['127.0.0.1'],port=9042)
@@ -24,7 +32,10 @@ session = cluster.connect()
 mnist_result_value = predictresult
 image_num_value = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 date_value = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-information_value = image_info
+
+
+
+#create the keyspace if it is not exist
 try:
     session.execute("""
        CREATE KEYSPACE %s
@@ -34,6 +45,10 @@ except:
     pass
     # keyspace already exist
 session.set_keyspace(KEYSPACE)
+
+
+# create the table if it is not exist
+# table for time image_name and result
 try:
     session.execute("""
         CREATE TABLE mnist_user_table (
@@ -46,6 +61,10 @@ try:
 except:
     pass
     # table already exist
+	
+
+# table for the name of image and the information of the image
+# the information is already be changed by the image_prepare
 try:
     session.execute("""
         CREATE TABLE image (
@@ -57,6 +76,8 @@ try:
 except:
     pass
 
+
+# submit the information of this image
 session.execute("""
 		INSERT INTO mnist_user_table (date, mnist_result, image_num) 
 		Values (%s,%s,%s)
